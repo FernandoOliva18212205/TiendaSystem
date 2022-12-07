819a225b61b294852450a2e09a22efcd2a50640f
felipec <te-lealc@sony-ste.sel.sony.com>
2022-12-07 13:39:30 -0800

    Arreglando menu

--- app/src/main/java/com/example/tiendasystem/MenuActivity.java
+++ app/src/main/java/com/example/tiendasystem/MenuActivity.java
@@ -8,41 +8,17 @@ import android.view.View;
 import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
-    Button btn1, btn2,btn3,btn4,btn5,btn6;
+    Button btn4,btn5,btn6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        btn1 = findViewById(R.id.button1);
-        btn2 = findViewById(R.id.button2);
-        btn3 = findViewById(R.id.button3);
         btn4 = findViewById(R.id.button4);
         btn5 = findViewById(R.id.button5);
         btn6 = findViewById(R.id.button6);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MenuActivity.this, productoCrear.class);
-                startActivity(intent);
-            }
-        });
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MenuActivity.this, productoEliminar.class);
-                startActivity(intent);
-            }
-        });
-        btn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MenuActivity.this, productoModificar.class);
-                startActivity(intent);
-            }
-        });
         btn4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
