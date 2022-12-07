edf52fb7d2bbf870be0e9244b430629c9926ba11
IngridMeza3320 <ingrid.meza18@tectijuana.edu.mx>
2022-12-07 12:03:00 -0800

    Intent iconos

--- app/src/main/java/com/example/tiendasystem/VerInventarioActivity.java
+++ app/src/main/java/com/example/tiendasystem/VerInventarioActivity.java
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.tiendasystem.db.adaptadores.listaProductos;
 import com.example.tiendasystem.db.dbProducto;
 import com.example.tiendasystem.db.entidades.productos;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class VerInventarioActivity extends AppCompatActivity {
     RecyclerView listProductos;
@@ -42,5 +47,32 @@ public class VerInventarioActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        switch (item.getItemId()){
+            case R.id.icon_add:{
+                Intent intent = new Intent(VerInventarioActivity.this, productoCrear.class);
+                startActivity(intent);
+                Toast.makeText(this, "Crear", Toast.LENGTH_LONG).show();
+                break;
+            }
+            case R.id.icon_save:{
+                Intent intent = new Intent(VerInventarioActivity.this, productoModificar.class);
+                startActivity(intent);
+                Toast.makeText(this, "Guardar", Toast.LENGTH_LONG).show();
+                break;
+            }
+            case R.id.icon_delete:{
+                Intent intent = new Intent(VerInventarioActivity.this, productoEliminar.class);
+                startActivity(intent);
+                Toast.makeText(this, "Eliminar", Toast.LENGTH_LONG).show();
+                break;
+            }
+            default: break;
+        }
+        return true;
     }
 }
